f12e1dd771c92a52969703225d88ee7483034e46
Taylor Picard <taylorkpicard1998@gmail.com>
2023-05-19 00:44:38 -0500

    Some done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+import Characters from './components/Character';
 
 const App = () => {
+  
+    const [data, setData] = useState();
+    useEffect(() => {
+      axios.get('https://swapi.dev/api/people/')
+        .then(res => {
+          console.log(res);
+          setData(res.data);
+        })
+        .catch(err => console.error(err))
+    }, []);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  /* 
+birth_year: 
+"19BBY"
+
+created: 
+"2014-12-09T13:50:51.644000Z"
+
+edited: 
+"2014-12-20T21:17:56.891000Z"
+
+eye_color: 
+"blue"
+
+films: 
+(4) ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi', 'Revenge of the Sith']
+
+gender: 
+"male"
+
+hair_color: 
+"blond"
+
+height: 
+"172"
+
+homeworld: 
+"http://swapi.dev/api/planets/1/"
+
+mass: 
+"77"
+
+name: 
+"Luke Skywalker"
+
+skin_color: 
+"fair"
+
+species: 
+[]
+
+starships: 
+(2) ['http://swapi.dev/api/starships/12/', 'http://swapi.dev/api/starships/22/']
+
+url: 
+"http://swapi.dev/api/people/1/"
+
+vehicles: 
+(2) ['http://swapi.dev/api/vehicles/14/', 'http://swapi.dev/api/vehicles/30/']
+
+  */
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -11,6 +76,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      { data && <Characters char={data}/>}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+
+import react from "react";
+
+const Characters = (props) => {
+    return (
+        <h1>{props.char.name}</h1>
+    )
+}
+
+export default Characters;
